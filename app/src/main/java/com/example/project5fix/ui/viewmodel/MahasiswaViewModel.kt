3d30045833eda2d6a53e148ebf6657411b678137
Alfandi Alamshach <fandixpc1@gmail.com>
2024-11-15 17:49:53 +0700

    Membuat fungsi di class MahasiswaViewModel

--- app/src/main/java/com/example/project5fix/ui/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/project5fix/ui/viewmodel/MahasiswaViewModel.kt
@@ -5,9 +5,25 @@ import com.example.project5fix.model.Mahasiswa
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class MahasiswaViewModel: ViewModel() {
     private val _uistate = MutableStateFlow(Mahasiswa())
 
     val uiState: StateFlow <Mahasiswa> = _uistate.asStateFlow()
+
+    fun saveDataMahasiswa(
+        ls : MutableList<String>
+    ){
+        _uistate.update{ data -> data.copy(
+            nama = ls[0],
+            gender = ls[1],
+            alamat = ls[2],
+            NIM = ls[3],
+            email = ls[4],
+            noHp = ls[5]
+                    )
+        }
+    }
 }
+
