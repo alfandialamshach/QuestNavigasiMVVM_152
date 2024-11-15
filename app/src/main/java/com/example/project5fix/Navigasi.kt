9bd5e20fdd01c4195f2d54d1c917ca1cb2188e69
Alfandi Alamshach <fandixpc1@gmail.com>
2024-11-15 18:25:37 +0700

    Menambahkan scaffold dalam Navigasi.kt

--- app/src/main/java/com/example/project5fix/Navigasi.kt
+++ app/src/main/java/com/example/project5fix/Navigasi.kt
@@ -1,10 +1,18 @@
 package com.example.project5fix
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+
+
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+
 import androidx.navigation.compose.rememberNavController
 import com.example.project5fix.ui.viewmodel.MahasiswaViewModel
 
@@ -18,4 +26,14 @@ fun Navigasi(
     modifier: Modifier = Modifier,
     viewModel: MahasiswaViewModel = viewModel(),
     navHost: NavHostController = rememberNavController()
-)
+){
+    Scaffold { isipadding ->
+        val uiState by viewModel.uistate.collectAsState()
+        NavHost (
+            modifier = modifier.padding(isipadding),
+            navController = navHost, startDestinantion = Halaman.Form.name
+        ){
+
+        }
+    }
+}
--- app/src/main/java/com/example/project5fix/ui/viewmodel/MahasiswaViewModel.kt
+++ app/src/main/java/com/example/project5fix/ui/viewmodel/MahasiswaViewModel.kt
@@ -4,13 +4,14 @@ import androidx.lifecycle.ViewModel
 import com.example.project5fix.model.Mahasiswa
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
 class MahasiswaViewModel: ViewModel() {
     private val _uistate = MutableStateFlow(Mahasiswa())
 
-    val uiState: StateFlow <Mahasiswa> = _uistate.asStateFlow()
+    val uistate: StateFlow<Mahasiswa> = _uistate.asStateFlow()
 
     fun saveDataMahasiswa(
         ls : MutableList<String>
