996eb2ceb9a6f3c2c27030c7803a22c20fa82ac5
Alfandi Alamshach <fandixpc1@gmail.com>
2024-11-15 17:56:55 +0700

    Membuat fungsi navigasi beserta parameter modifier

--- app/src/main/java/com/example/project5fix/Navigasi.kt
+++ app/src/main/java/com/example/project5fix/Navigasi.kt
@@ -1,6 +1,21 @@
 package com.example.project5fix
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.project5fix.ui.viewmodel.MahasiswaViewModel
+
 enum class Halaman {
     Form,
     Data
 }
+
+@Composable
+fun Navigasi(
+    modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController()
+)
