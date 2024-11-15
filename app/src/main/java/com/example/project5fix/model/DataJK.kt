9ea116a08997f5cd73ee009cd6f6e9370b757a68
Alfandi Alamshach <fandixpc1@gmail.com>
2024-11-15 17:17:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/project5fix/model/DataJK.kt
@@ -0,0 +1,10 @@
+package com.example.project5fix.model
+
+import com.example.project5fix.R
+
+object DataJK {
+    val isiJk = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
---
+++ app/src/main/java/com/example/project5fix/model/Mahasiswa.kt
@@ -0,0 +1,10 @@
+package com.example.project5fix.model
+
+data class Mahasiswa(
+    val nama : String = "",
+    val gender : String = "",
+    val alamat : String = "",
+    val NIM : String = "",
+    val email : String = "",
+    val noHp : String = ""
+)
